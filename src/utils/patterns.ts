2858b01a8fb34ecaf4b0accadc009fd31b0f053f
Lucas <lucasmartinez.it@gmail.com>
2024-03-28 21:47:14 +0100

    update start script

--- src/utils/patterns.ts
+++ src/utils/patterns.ts
@@ -1,8 +1,8 @@
 /**
- * The patterns are the same than the ones on the app
+ * The patterns are the same as the ones on the app
  */
 
-export const passwordPattern = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/
+export const passwordPattern = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?\d).{7,})\S$/
 
 export const emailPattern = /^([\w-+]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
 
