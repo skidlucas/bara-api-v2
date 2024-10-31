0cbed38ec190c41c315745e2d1155bdb1bd17981
lucas <lucasmartinez.it@gmail.com>
2024-10-31 09:56:36 +0100

    fix

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -1,5 +1,5 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common'
-import e, { Request, Response } from 'express'
+import { Request, Response } from 'express'
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
